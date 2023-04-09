d853c42b1cac2c3c7ca0204d6cfdc1605cf39173
tarmidzibariq <tarmidzibariq30@gmail.com>
2023-04-09 16:04:07 +0700

    add useScrollAnchor and useModalDOM

--- src/assets/pages/HomePage.js
+++ src/assets/pages/HomePage.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Header from "assets/parts/Header"
-import Hero from "assets/parts/Hero";
+import Hero from "assets/parts/HomePage/Hero";
 import BrowseRoom from "assets/parts/HomePage/BrowseRoom";
 import JustArrived from "assets/parts/HomePage/JustArrived";
 import Clients from "assets/parts/Clients";
 import Sitemap from "assets/parts/Sitemap";
 import Footer from "assets/parts/Footer";
+
+import useScrollAnchor from "assets/helpers/hooks/useScrollAnchor";
+import useModalDOM from "assets/helpers/hooks/useModalDOM";
 export default function HomePage() {
+    useScrollAnchor();
+    useModalDOM();
+
     return (
         <>
             <Header theme="white" position="absolute"/>
--- src/assets/parts/HomePage/Hero.js
+++ src/assets/parts/HomePage/Hero.js
@@ -22,8 +22,8 @@ export default function Hero() {
             <div className="relative hero-image">
               <div className="overlay inset-0 bg-black opacity-35 z-10"></div>
               <div className="overlay right-0 bottom-0 md:inset-0">
-                <button className="video hero-cta focus:outline-none z-30 modal-trigger" data-content='<div className="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
-              <div className="absolute w-full h-full">
+                <button className="video hero-cta focus:outline-none z-30 modal-trigger" data-content='<div class="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
+              <div class="absolute w-full h-full">
                 <iframe
                   width="100%"
                   height="100%"
